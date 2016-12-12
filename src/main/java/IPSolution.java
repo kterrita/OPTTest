ff8b864a55c1e521e82b5928e86ef1073886e3ad
kterrita <kterrita@gmail.com>
2016-12-12 12:49:00 +0300

    Initial commit

---
+++ src/main/java/IPSolution.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by илья on 12.12.2016.
+ */
+public class IPSolution {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstIP = reader.readLine();
+        String secondIP = reader.readLine();
+
+
+    }
+}
