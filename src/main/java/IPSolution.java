b85ad338f101e01f37ceff011dce4328daf7d4c5
kterrita <kterrita@gmail.com>
2016-12-12 13:26:43 +0300

    First Try

--- src/main/java/IPSolution.java
+++ src/main/java/IPSolution.java
@@ -5,11 +5,52 @@ import java.io.InputStreamReader;
  * Created by илья on 12.12.2016.
  */
 public class IPSolution {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String firstIP = reader.readLine();
         String secondIP = reader.readLine();
 
+        printIpRange(firstIP, secondIP);
+    }
 
+    private static void printIpRange(String startIp, String endIP) {
+        long start = ipToLong(startIp);
+        long end = ipToLong(endIP);
+
+        for (long i = start + 1; i < end; i++) {
+            System.out.println(longToIp(i));
+        }
+    }
+
+    private static long ipToLong(String IPAddress) {
+        long result = 0;
+
+        String[] ipArray = IPAddress.split("\\.");
+
+        for (int i = 3; i >= 0; i--) {
+
+            long ip = Long.parseLong(ipArray[3 - i]);
+
+            result |= ip << (i * 8);
+        }
+
+        return result;
+    }
+
+    private static String longToIp(long ip) {
+        StringBuilder result = new StringBuilder(15);
+
+        for (int i = 0; i < 4; i++) {
+
+            result.insert(0, Long.toString(ip & 0xff));
+
+            if (i < 3) {
+                result.insert(0, '.');
+            }
+
+            ip = ip >> 8;
+
+        }
+        return result.toString();
     }
 }
